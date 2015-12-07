31d798e21709632f6c586c727e88124dc59091e9
Duncan McIsaac <dbmcisaac1@gmail.com>
2015-12-07 17:03:58 -0500

    Initial Commit

---
+++ Cuppa/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Cuppa
+//
+//  Created by Duncan McIsaac on 12/7/15.
+//  Copyright (c) 2015 Duncan McIsaac. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
